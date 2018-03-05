d6eea7ad35d9446fb6bd88e384de525bd5672cab
Ashish Gupta <akvns152@gmail.com>
2018-03-05 09:45:13 +0530

    head content updated

--- pages/co-ordinator-panel.php
+++ pages/co-ordinator-panel.php
@@ -78,7 +78,19 @@
 
   <div class="main-cont">
      <div class="line"></div>
-    <h1>ARK 2K18</h1>
+    <img src="../images/biglogo.png" class="aktu">
+
+    <h1 class="head-expanded">
+    NOIDA INSTITUTE OF ENGINEERING AND TECHNOLOGY  
+    </h1>
+
+    
+
+    <img src="../images/niet.png" class="aktu2">
+    <h5 class="subhead-expanded">
+      Accredited by NAAC (A Grade) CSE, ECE, ME, &amp; B.Pharm are NBA accredited 99th Rank by NIRF (2016)
+    </h5>
+    <h1 class="head-compressed">NIET, GR. NOIDA</h1>
     <center><div class="line_2"></div></center>
     <div class="menu-2">
         <a href="../index.php">HOME | </a>
