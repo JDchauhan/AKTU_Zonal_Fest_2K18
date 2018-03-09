899e3d0aa95e92ec63266c8618c1d07ef15d1aac
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-03-09 06:13:32 +0530

    front-end validation

--- js/main.js
+++ js/main.js
@@ -89,27 +89,56 @@ waves: [
 function validateForm() 
 {   
     var clg_name=document.forms["reg-form"]["clg_name"].value;
+    var clg_code=132;
+    var cordinator_name = document.forms["reg-form"]["co-ordinator_name"].value;
+    var cordinator_email = document.forms["reg-form"]["co-ordinator_email"].value;
+    var cordinator_mobile = document.forms["reg-form"]["co-ordinator_mob_no"].value;
+    
     var event=document.forms["reg-form"]["event"].value;
-    /*
-    var name_1 = document.forms["reg-form"]["name"][0].value;
-    var email_1=document.forms["reg-form"]["email_1"].value;
-    var roll_no_1=document.forms["reg-form"]["roll_no_1"].value;
-    var mob_no_1=document.forms["reg-form"]["mob_no_1"].value;
-    var name_2 = document.forms["reg-form"]["name_2"].value;
-    var email_2=document.forms["reg-form"]["email_2"].value;
-    var roll_no_2=document.forms["reg-form"]["roll_no_2"].value;
-    var mob_no_2=document.forms["reg-form"]["mob_no_2"].value;
     var participants=document.forms["reg-form"]["no_of_participants"].value;
-    */
+    participants = 1;
+    
+    var name = [], email= [], mob_no= [], roll_no=[];
+    name[0] = document.forms["reg-form"]["name[0]"].value;
+    email[0] = document.forms["reg-form"]["email[0]"].value;
+    roll_no[0] = document.forms["reg-form"]["roll_no[0]"].value;
+    mob_no[0] = document.forms["reg-form"]["mob_no[0]"].value;
+
+    name[1] = document.forms["reg-form"]["name[1]"].value;
+    email[1] = document.forms["reg-form"]["email[1]"].value;
+    roll_no[1] = document.forms["reg-form"]["roll_no[1]"].value;
+    mob_no[1] = document.forms["reg-form"]["mob_no[1]"].value;
 
 
-    if(participants==2)
-    {
-    if(clg_name== "")
-    {
+    name[2] = document.forms["reg-form"]["name[2]"].value;
+    email[2] = document.forms["reg-form"]["email[2]"].value;
+    roll_no[2] = document.forms["reg-form"]["roll_no[2]"].value;
+    mob_no[2] = document.forms["reg-form"]["mob_no[2]"].value;
+
+
+    name[3] = document.forms["reg-form"]["name[3]"].value;
+    email[3] = document.forms["reg-form"]["email[3]"].value;
+    roll_no[3] = document.forms["reg-form"]["roll_no[3]"].value;
+    mob_no[3] = document.forms["reg-form"]["mob_no[3]"].value;
+
+
+
+    if(clg_name == ""){
         document.getElementById("message").className="";
         document.getElementById("message").className="error";
         document.getElementById("message").innerHTML="PLEASE ENTER COLLEGE NAME";
+        return false;
+    }
+    else if(clg_code == "" ){
+        document.getElementById("message").className="";
+        document.getElementById("message").className="error";
+        document.getElementById("message").innerHTML="PLEASE ENTER COLLEGE CODE";
+        return false;
+    }
+    else if(cordinator_name == ""){
+        document.getElementById("message").className="";
+        document.getElementById("message").className="error";
+        document.getElementById("message").innerHTML="PLEASE ENTER COLLEGE CORDINATOR NAME";
         return false;
     }
     else if(event=="")
@@ -119,95 +148,68 @@ function validateForm()
         document.getElementById("message").innerHTML="PLEASE CHOOSE A EVENT";
         return false;
     }
-    else if (name_1 == "" || name_2 == "") 
+    else if(cordinator_email == "")
     {
         document.getElementById("message").className="";
         document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE ENTER YOUR NAME";
+        document.getElementById("message").innerHTML="PLEASE ENTER CORDINATOR'S EMAIL";
         return false;
     }
-    else if(email_1== ""||email_2=="")
+    else if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(cordinator_email)))
     {
         document.getElementById("message").className="";
         document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE ENTER YOUR EMAIL";
+        document.getElementById("message").innerHTML="PLEASE ENTER CORRECT CORDINATOR EMAIL";
         return false;
     }
-    else if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email_1))||!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email_2)))
-    {
-        document.getElementById("message").className="";
-        document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE ENTER CORRECT EMAIL";
-        return false;
-    }
-    else if(isNaN(roll_no_1) || isNaN(roll_no_2) || roll_no_1.length < 1 || roll_no_2.length < 1)
-    {
-        document.getElementById("message").className="";
-        document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE ENTER CORRECT ROLL NUMBER";
-        return false;
-    }
-    else if(mob_no_1.length != 10 || isNaN(mob_no_2) || mob_no_2.length != 10 || isNaN(mob_no_2))
-    {
-        document.getElementById("message").className="";
-        document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE CORRECT MOBILE NUMBER";
-        return false;
-    }
-}
-else
-{
-    if(clg_name== "")
-    {
-        document.getElementById("message").className="";
-        document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE ENTER COLLEGE NAME";
-        return false;
-    }
-    else if(event=="")
-    {
-        document.getElementById("message").className="";
-        document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE CHOOSE A EVENT";
-        return false;
-    }
-    else if (name_1 == "") 
-    {
-        document.getElementById("message").className="";
-        document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE ENTER YOUR NAME";
-        return false;
-    }
-    else if(email_1== "")
-    {
-        document.getElementById("message").className="";
-        document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE ENTER YOUR EMAIL";
-        return false;
-    }
-    else if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email_1)))
-    {
-        document.getElementById("message").className="";
-        document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE ENTER CORRECT EMAIL";
-        return false;
-    }
-    else if(isNaN(roll_no_1) || roll_no_1.length < 1)
-    {
-        document.getElementById("message").className="";
-        document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE ENTER CORRECT ROLL NUMBER";
-        return false;
-    }
-    else if(mob_no_1.length != 10 || isNaN(mob_no_1))
-    {
-        document.getElementById("message").className="";
-        document.getElementById("message").className="error";
-        document.getElementById("message").innerHTML="PLEASE CORRECT MOBILE NUMBER";
-        return false;
+    else if(cordinator_mobile.length != 10 || isNaN(cordinator_mobile) )
+        {
+            document.getElementById("message").className="";
+            document.getElementById("message").className="error";
+            document.getElementById("message").innerHTML="PLEASE CORRECT CORDINATOR MOBILE NUMBER";
+            return false;
+        }
+
+    
+    for(var i = 0; i< participants; i++){
+        if (name[i] == "") 
+        {
+            document.getElementById("message").className="";
+            document.getElementById("message").className="error";
+            document.getElementById("message").innerHTML="PLEASE ENTER YOUR NAME";
+            return false;
+        }
+        else if(email[i] == "")
+        {
+            document.getElementById("message").className="";
+            document.getElementById("message").className="error";
+            document.getElementById("message").innerHTML="PLEASE ENTER YOUR EMAIL";
+            return false;
+        }
+        else if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email[i])))
+        {
+            document.getElementById("message").className="";
+            document.getElementById("message").className="error";
+            document.getElementById("message").innerHTML="PLEASE ENTER CORRECT EMAIL";
+            return false;
+        }
+        else if(isNaN(roll_no[i]) || roll_no[i].length < 1)
+        {
+            document.getElementById("message").className="";
+            document.getElementById("message").className="error";
+            document.getElementById("message").innerHTML="PLEASE ENTER CORRECT ROLL NUMBER";
+            return false;
+        }
+        else if(mob_no[i].length != 10 || isNaN(mob_no[i]) )
+        {
+            document.getElementById("message").className="";
+            document.getElementById("message").className="error";
+            document.getElementById("message").innerHTML="PLEASE CORRECT MOBILE NUMBER";
+            return false;
+        }    
     }
 }
-}
+
 function disable()
 {
     var select=document.getElementById("select").value;
