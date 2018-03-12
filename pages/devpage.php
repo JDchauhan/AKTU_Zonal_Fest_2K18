5ccde6a602b4261706c5cb812a960b5c78f1b843
Ashish Gupta <akvns152@gmail.com>
2018-03-12 21:22:33 +0530

    devpage adjusted

--- pages/devpage.php
+++ pages/devpage.php
@@ -123,7 +123,7 @@
     <div class="success hidden" id="message">
       Success MESSAGE
     </div>
-    <div class="col-sm-12 dev-cont">
+    <div class="col-sm-12 dev-cont" style="height: 500px; overflow-y: auto;padding-bottom: 200px;">
      <div class="row">
        <div class="col-sm-6">
         <div  class="content">
